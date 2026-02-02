593e956dcab967b0dc2f9a851d66ba8919bd7fca
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-02-02 21:02:46 +0530

    about nav

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,35 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={{ ...styles.container }}
     >
-      <Text>WellCome Nethmal !</Text>
+      <Text style={{ ...styles.content }}>Hello Geesara !</Text>
+      <Link style={{...styles.link}} href={"/about"}>Visit about Seen</Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({ 
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "black",
+    gap: 10,
+  },
+
+  content: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "white",
+  },
+
+  link:{
+    fontSize:16,
+    color:"blue"
+  }
+});
+  
